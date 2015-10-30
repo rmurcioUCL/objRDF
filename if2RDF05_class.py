39e1caad0ed7b07991255778784a8a6192a61ece
Patrick Tantra <p.tantra@ucl.ac.uk>
2015-10-30 15:15:48 +0000

    Update if2RDF05_class.py

--- if2RDF05_class.py
+++ if2RDF05_class.py
@@ -1,11 +1,7 @@
 # coding=utf-8
 __author__ = 'patrick'
-__author__ = 'Wick 1'
 __author__ = 'casa'
-# -*- coding: utf-8 -*-
-#libraries
-#import tkinter as tk
-#from tkinter import filedialog
+
 import csv
 import uuid
 import pyproj
@@ -40,7 +36,7 @@ class RDF:
         self.schema = Namespace("http://schema.org/")
         self.naptan = Namespace("http://transport.data.gov.uk/def/naptan/")
         self.xsd = Namespace("http://www.w3.org/2001/XMLSchema#")
-        self.dfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
+        self.rdfs = Namespace("http://www.w3.org/2000/01/rdf-schema#")
         self.locationOnt = Namespace("http://data.linkedevents.org/def/location#")
         self.geo = Namespace("http://www.w3.org/2003/01/geo/wgs84_pos#")
         self.geosparql = Namespace("http://www.opengis.net/ont/geosparql#")
@@ -55,10 +51,9 @@ class RDF:
         self.g = Graph(self.store)
         self.graph = ConjunctiveGraph(self.store)
 
-
 class Train(RDF):
     def __init__(self, stationId, stationLat, stationLong, stationGeometry, route, title,
-           stationBusinessType, stationPublisher, trainId, trainWkt, stationGUID):
+                 trainId, trainWkt, stationGUID):
 
         RDF.__init__(self)
         self.stationId=stationId
@@ -67,8 +62,8 @@ class Train(RDF):
         self.stationGeometry=stationGeometry
         self.route=route
         self.title=title
-        self.stationBusinessType=stationBusinessType
-        self.stationPublisher=stationPublisher
+        self.stationBusinessType=URIRef('http://data.linkedevents.org/kos/3cixty/trainstation')
+        self.stationPublisher=URIRef('https://tfl.gov.uk/modes/trains/')
         self.trainId=trainId
         self.trainWkt=trainWkt
         self.stationGUID=stationGUID
@@ -87,62 +82,164 @@ class Train(RDF):
         return singleStation
 
     def createTransitRoute(self):
-        transitRoute = URIRef("http://data.linkedevents.org/transit/London/railwayRoute/%s") % self.route.replace(" ", "-").lower()
+        transitRoute = URIRef("http://data.linkedevents.org/transit/London/railwayRoute/%s") % self.route#.replace(" ", "-").lower()
         return transitRoute
 
     def createLine(self):
-        stationId = URIRef('http://data.linkedevents.org/transit/London/trainLine/' + self.stationId)
+        stationId = URIRef('http://data.linkedevents.org/transit/London/trainLine/' + Literal(self.stationId))
         return stationId
 
     def createRoute(self):
-        route = URIRef('http://data.linkedevents.org/transit/London/route/' + self.route)
-        return route
+        trainRoute = URIRef('http://data.linkedevents.org/transit/London/route/' + self.route)
+        return trainRoute
 
     def createTrainGraph(self):
         singleStation = self.createTrainStation() #"http://data.linkedevents.org/transit/London/station/" + Literal(stationTitle)
         transitRoute =  self.createTransitRoute()
-        singleGeometry= URIRef("http://data.linkedevents.org/location/" + "%s" + "/geometry") % self.title
+        singleGeometry= URIRef("http://data.linkedevents.org/location/" + "%s" + "/geometry") % Literal(self.title)
+        singleLine=self.createLine()
         #singleGeometry = createGeometry(arg[5]) #"http://data.linkedevents.org/location/" + Literal(stationId) + "/geometry"
 
-        self.g.add(singleStation, self.rdf.type, self.naptan.RailwayStation)
-        self.g.add(singleStation, self.rdf.type, self.dul.Place)
-        self.g.add(singleStation, self.rdf.type, self.transit.Stop)
-        self.g.add(singleStation, self.dc.identifier, Literal(self.stationId))
-        self.g.add(singleStation, self.rdfs.label, Literal(self.title)) #g.add((singleStation, rdfs.label, arg[3]))
+        self.g.add((singleStation, self.rdf.type, self.naptan.RailwayStation))
+        self.g.add((singleStation, self.rdf.type, self.dul.Place))
+        self.g.add((singleStation, self.rdf.type, self.transit.Stop))
+        self.g.add((singleStation, self.dc.identifier, Literal(self.stationId)))
+        self.g.add((singleStation, self.rdfs.label, Literal(self.title))) #g.add((singleStation, rdfs.label, arg[3]))
 
-        self.g.add(singleGeometry, self.rdf.type, geo.Point)
-        self.g.add(singleGeometry, self.geo.lat, Literal(self.stationLat, datatype=xsd.double))
-        self.g.add(singleGeometry, self.geo.long, Literal(self.stationLong, datatype=xsd.double))
-        self.g.add(singleGeometry, self.locn.geometry, Literal(self.stationGeometry, datatype=geosparql.wktLiteral))
-        self.g.add(singleStation, self.transit.route, transitRoute)
-        self.g.add(singleStation, self.schema.name, Literal(self.route))#NEW
-        self.g.add(singleStation, self.geo.location, Literal(singleGeometry))#g.add((singleStation, schema.location, singleAddress))
-        self.g.add(singleStation, self.dc.publisher, Literal(self.stationPublisher))
-        self.g.add(singleStation, self.locationOnt.businessType, Literal(self.stationBuinessType))
+        self.g.add((singleGeometry, self.rdf.type, self.geo.Point))
+        self.g.add((singleGeometry, self.geo.lat, Literal(self.stationLat, datatype=self.xsd.double)))
+        self.g.add((singleGeometry, self.geo.long, Literal(self.stationLong, datatype=self.xsd.double)))
+        self.g.add((singleGeometry, self.locn.geometry, Literal(self.stationGeometry, datatype=self.geosparql.wktLiteral)))
+        self.g.add((singleStation, self.transit.route, transitRoute))
+        self.g.add((singleStation, self.schema.name, Literal(self.route)))#NEW
+        self.g.add((singleStation, self.geo.location, Literal(singleGeometry)))#g.add((singleStation, schema.location, singleAddress))
+        self.g.add((singleStation, self.dc.publisher, Literal(self.stationPublisher)))
+        self.g.add((singleStation, self.locationOnt.businessType, Literal(self.stationBusinessType)))
 
-        #TRAIN LINES GRAPH
-
-        singleLine=self.createLine()
-
-        self.g.add(singleLine, self.rdf.type, self.transit.RailRoute) #trainroute to trainroute
-        self.g.add(singleLine, self.transit.route, createRoute(self.route))
-        self.g.add(createTransitRoute(self.route), schema.name, Literal(self.title))
+        self.g.add((singleLine, self.rdf.type, self.transit.Railroute))
+        self.g.add((singleLine, self.transit.route, self.createRoute()))
+        self.g.add((self.createRoute(), self.schema.name, Literal(self.title)))
 
         return self.g
 
+ts_init = Train('0000','-0.0001','0.0001','X','route0' ,'stationA' , '0', 'POINT(-0.001 0.001)', '0000X')
 
-stationId=['0000','1001', '2002', '3003', '4004']
-stationLat=['-0.0001',' -0.10001', '-0.20002', '-0.3003', '-0.4004']
-stationLong=['0.0001', '0.10001', '0.20002', '0.3003', '0.4004']
-stationGeometry=[]
-route=['route0', 'route1', 'route2', 'route3', 'route4']
-title=['stationA', 'stationB', 'stationC', 'stationD', 'stationE']
-stationBusinessType=URIRef('http://data.linkedevents.org/kos/3cixty/trainstation')
-stationPublisher=URIRef('https://tfl.gov.uk/modes/trains/')
-trainWkt=['POINT(-0.001 0.001)', 'POINT(-0.002 0.002)', 'POINT(-0.003 0.003)', 'POINT(-0.004 0.004)', 'POINT(-0.005 0.005)']
-stationGUID=['0000X', '1001X', '2002X', '3003X', '4004X']
 
-trainStation = ['0000','-0.0001','0.0001','X','route0' ,'stationA' , 'URI', 'URI', '0', 'POINT(-0.001 0.001)', '0000X']
-#print createTrainGraph(trainStation).serialize(format='turtle')
-print Train('0000','-0.0001','0.0001','X','route0' ,'stationA' , 'URI', 'URI', '0', 'POINT(-0.001 0.001)', '0000X')
-print stationId[0], stationLat[0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
